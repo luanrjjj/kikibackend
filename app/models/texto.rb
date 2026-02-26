6590104e5185c0672493f14789aa576c4bf0aae0
Luan Alfredo <luanfreitas12@poli.ufrj.br>
2026-02-26 14:42:38 -0300

    Add Texto model with associations and custom JSON serialization

--- app/models/texto.rb
+++ app/models/texto.rb
@@ -1,2 +1,9 @@
 class Texto < ApplicationRecord
+  belongs_to :prova
+  belongs_to :concurso
+  has_many :questaos
+
+  def as_json(options = {})
+    super(options.merge(except: [:created_at, :updated_at]))
+  end
 end
