be6e51e704a50dc110e48abb7e540ea18a23efdf
Luan Alfredo <luanfreitas12@poli.ufrj.br>
2026-03-12 23:44:43 -0300

    try fix prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  Rails.application.routes.default_url_options[:host] = 'apoloconcursos.com.br'
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
