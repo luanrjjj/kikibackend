b3402f3c0198f93851fc73d8425fc282e9042464
Luan Alfredo <luanfreitas12@poli.ufrj.br>
2026-03-14 14:46:53 -0300

    fix prod log

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,12 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    config.logger = ActiveSupport::Logger.new(STDOUT)
+      .tap  { |logger| logger.formatter = config.log_formatter }
+      .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+  end
   $stdout.sync = true
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
