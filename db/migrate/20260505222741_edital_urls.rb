91e40fbb4d8c74e0e79eb4fb9753410a14257f9c
Luan Alfredo <luanfreitas12@poli.ufrj.br>
2026-05-05 19:31:30 -0300

    feat: add new URL columns to provas and concursos tables

--- db/migrate/20260505222741_edital_urls.rb
+++ db/migrate/20260505222741_edital_urls.rb
@@ -1,4 +1,7 @@
 class EditalUrls < ActiveRecord::Migration[8.0]
   def change
+      add_column :provas, :edital_url, :string
+      add_column :provas, :prova_url, :string
+      add_column :concursos, :pdf_folder_url, :string
   end
 end
