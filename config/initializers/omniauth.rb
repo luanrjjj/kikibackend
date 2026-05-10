f0d4eeaa6dfc127c99aa821b1b26f979fd053624
Luan Alfredo <luanfreitas12@poli.ufrj.br>
2026-05-10 10:18:02 -0300

    feat: add OmniAuth configuration for Google OAuth2 with API path prefix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.path_prefix = '/api/auth'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET_ID']
   # provider :microsoft_graph, ENV['MICROSOFT_CLIENT_ID'], ENV['MICROSOFT_CLIENT_SECRET']
