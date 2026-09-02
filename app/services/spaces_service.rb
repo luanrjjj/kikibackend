dbdc374bad03bf4e88dc2e60d9cc272a8991b26c
Luan Alfredo <luanfreitas12@poli.ufrj.br>
2026-09-02 00:40:05 -0300

    feat: refactor SpacesService to improve S3 client configuration and URL generation

--- app/services/spaces_service.rb
+++ app/services/spaces_service.rb
@@ -1,24 +1,30 @@
 require 'aws-sdk-s3'
 
 class SpacesService
+  CDN_BASE_URL = (ENV['CDN_SPACES_URL'] || 'https://arquivos.apoloconcursos.com.br').gsub(/\/+$/, '')
+
   def self.client
     @client ||= Aws::S3::Client.new(
-      access_key_id: ENV['SPACES_ACCESS_KEY'],
-      secret_access_key: ENV['SPACES_SECRET_KEY'],
-      endpoint: ENV['SPACES_ENDPOINT_URL'],
-      region: ENV['SPACES_REGION']
+      access_key_id: ENV['SPACES_ACCESS_KEY'] || ENV['SPACES_KEY'],
+      secret_access_key: ENV['SPACES_SECRET_KEY'] || ENV['SPACES_SECRET'],
+      endpoint: ENV['SPACES_ENDPOINT_URL'] || ENV['SPACES_ENDPOINT'] || 'https://sfo3.digitaloceanspaces.com',
+      region: ENV['SPACES_REGION'] || 'sfo3'
     )
   end
 
+  def self.cdn_url(key)
+    clean_key = key.to_s.sub(/\A\/+/, '')
+    "#{CDN_BASE_URL}/#{clean_key}"
+  end
+
   def self.create_folder(folder_name)
-    bucket = ENV['SPACES_BUCKET_NAME']
+    bucket = ENV['SPACES_BUCKET_NAME'] || ENV['SPACES_BUCKET'] || 'kikiproject'
     # S3 folders are just objects with a trailing slash
     key = "concursos_pdfs/#{folder_name}/"
 
     begin
       client.put_object(bucket: bucket, key: key)
-      # Return the "URL" - technically it's a path, but we can provide the full URL if needed
-      "#{ENV['SPACES_ENDPOINT']}/#{bucket}/#{key}"
+      cdn_url(key)
     rescue StandardError => e
       Rails.logger.error "SpacesService#create_folder Error: #{e.message}"
       raise e
@@ -26,17 +32,17 @@ class SpacesService
   end
 
   def self.upload_file(key, file)
-    bucket = ENV['SPACES_BUCKET_NAME']
+    bucket = ENV['SPACES_BUCKET_NAME'] || ENV['SPACES_BUCKET'] || 'kikiproject'
 
     begin
       client.put_object(
         bucket: bucket,
         key: key,
-        body: file.read,
+        body: file.respond_to?(:read) ? file.read : file,
         acl: 'public-read',
-        content_type: file.content_type
+        content_type: file.respond_to?(:content_type) ? file.content_type : 'application/octet-stream'
       )
-      "#{ENV['SPACES_ENDPOINT']}/#{bucket}/#{key}"
+      cdn_url(key)
     rescue StandardError => e
       Rails.logger.error "SpacesService#upload_file Error: #{e.message}"
       raise e
